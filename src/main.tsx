f7e68a04398693f3c820c8682524d9ed376c236b
Theogama <sandilegama.sg@gmail.com>
2026-05-15 20:49:36 +0200

    implemented vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/react";
+import { Analytics } from "@vercel/analytics/react"
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -15,5 +16,7 @@ createRoot(document.getElementById("root")!).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
     </ClerkProvider>
+    <Analytics />
   </StrictMode>
+  
 );
